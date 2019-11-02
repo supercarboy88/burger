ba65cab485624ab24a0a6cb6d5ea9ca75ed20851
YI YING <supercarboy88@gmail.com>
2019-11-02 02:14:39 -0700

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "your password",
-  database: "burgers_db"
-});
+var connection;
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  })
+}
 
 // Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
